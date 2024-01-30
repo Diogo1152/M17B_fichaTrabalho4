e1b0b7c371a4af33ab5de720ce7aedba044bf29d
Diogo Lança <a17139@d1-08-06.dped.local>
2024-01-30 12:12:32 +0000

    Atualizar o repositorio, getter e setters, javadoc comments

--- src/main/java/psi_11_o/m17b_fichatrabalho4/Ponto2D.java
+++ src/main/java/psi_11_o/m17b_fichatrabalho4/Ponto2D.java
@@ -4,37 +4,24 @@ public class Ponto2D {
     private int x;
     private int y;
     
-    /**
-     * 
-     */
+
     public Ponto2D(){
         x = 0;
         y = 0;        
     }
     
-    /**
-     * 
-     * @param x
-     * @param y 
-     */
+
     public Ponto2D(int x, int y){
         this.x = x;
         this.y = y;        
     }
     
-    /**
-     * 
-     * @param a
-     * @return 
-     */
+
     public double distancia(Ponto2D a){
         return Math.abs(Math.sqrt(Math.pow((x-a.x),2)+Math.pow((y-a.y),2)));
     }
     
-        /**
-     * 
-     * @return 
-     */
+
     @Override
     public String toString() {
         return "Ponto2D[" + x + "," + y + "]";
